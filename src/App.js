4fb0a6458fb88f91e978e5543fa934f84d499f32
Balaji <kbalaji880@gmail.com>
2023-08-15 16:24:52 +0530

    Back end integration

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios'
 
 function App() {
+
+  useEffect(()=>{
+    axios.get('https://8a8zekrutl.execute-api.ap-south-1.amazonaws.com/prod/books').then((res)=>{
+      console.log(res)
+    })
+  },[])
+  
   return (
     <div className="App">
       Testing the data
